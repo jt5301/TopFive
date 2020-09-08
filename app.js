501394e0b9152c2c0c5f8a1efc554511232a3fdc
John Tieu <johnson.tieu@gmail.com>
2020-09-08 04:36:24 -0400

    heroku path join

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.use("/movies", indexRouter);
 
