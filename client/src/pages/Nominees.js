2f80d2918121de346de44ba155d9f91533bacf81
John Tieu <johnson.tieu@gmail.com>
2020-09-26 01:57:18 -0400

    added button to pop up after 5 nominees

--- client/src/pages/Nominees.js
+++ client/src/pages/Nominees.js
@@ -4,6 +4,13 @@ import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
 import { SearchContext } from './SearchContext'
 import { MovieCard } from './MovieCard.js'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex'
@@ -20,6 +27,7 @@ const Nominees = () => {
   let movieParam = useContext(SearchContext)
   const classes = useStyles();
   const [instructions, setInstructions] = useState(true)
+  const [saveButton, triggerSaveButton] = useState(true)
   useEffect(() => {
     const movies = JSON.parse(localStorage.getItem('storedNominees')) || ""
     movieParam.setNominee(movies)
@@ -27,13 +35,21 @@ const Nominees = () => {
     localStorage.setItem('storedNominees', stringMovies)
   }, [])
   useEffect(() => {
+    let nomineeCount = 0
     let noNominees = true
     Object.keys(movieParam.nominees).forEach((current) => {
-      if (movieParam.nominees[current]) noNominees = false
+      if (movieParam.nominees[current]) {
+        nomineeCount += 1
+        noNominees = false
+      }
     })
+
+    if (nomineeCount < 5) triggerSaveButton(false)
+    else triggerSaveButton(true)
     if (noNominees) setInstructions(true)
     else setInstructions(false)
   }, [movieParam.nominees])
+
   return (
     <Container
       className={classes.cardGrid} maxwidth='xl'
@@ -54,6 +70,10 @@ const Nominees = () => {
             }
             else return ''
           })}
+      {saveButton ?
+        <Button variant="outlined" color="primary" >
+          Open form dialog
+      </Button> : ""}
     </Container>
   )
 }
--- routes/movies.js
+++ routes/movies.js
@@ -6,7 +6,6 @@ const omdbRootUrl = 'http://www.omdbapi.com/?'
 const apiKey = `&apikey=9251ce8f`//enter key here
 
 router.get("/search/:keywords", async (req, res, next) => {
-
   const movie = await axios.get(omdbRootUrl + `s=${req.params.keywords}&type=movie` + apiKey)
   res.status(200).json(movie.data);
 
